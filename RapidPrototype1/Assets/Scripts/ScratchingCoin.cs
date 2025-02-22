e3aa942047668486223d6e51627f09d2090676a1
Basketbirdy <danielmb0045@gmail.com>
2025-02-22 15:15:50 +0100

    Tweaks after playtesting - Added slight delay to scratching - Added new entity sound - Tweaked ending text

--- RapidPrototype1/Assets/Scripts/ScratchingCoin.cs
+++ RapidPrototype1/Assets/Scripts/ScratchingCoin.cs
@@ -1,10 +1,18 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class ScratchingCoin : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] private float scratchTime = .3f;
+
+    [Space]
+
+    [SerializeField] private float elapsedTime;
 
     private IDragable dragable;
+    private Coroutine scratchDelay;
+    private bool isCoroutineRunning = false;
 
     private void Awake()
     {
@@ -22,15 +30,45 @@ public class ScratchingCoin : MonoBehaviour
         if (scratchable == null) { return; }
         if (!dragable.IsDragged) { return; }
         if (scratchable.IsScratched) { return; }
-        
-        if(coverable != null)
+
+        if (coverable != null)
         {
             if (coverable.CheckIfCovered()) { return; }
         }
 
-        if(scratchable.IsScratched) { return; }
-        scratchable.OnScratch();
+        if (!isCoroutineRunning)
+        {
+            isCoroutineRunning = true;
+            scratchDelay = StartCoroutine(Timer(scratchTime, scratchable.OnScratch));
+            Debug.Log($"Coroutine; Started: {scratchDelay}");
+        }
+    }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!isCoroutineRunning) { return; }
 
+        IScratchable scratchable = collision.GetComponent<IScratchable>();
+        if (scratchable == null) { return; }
+
+        StopCoroutine(scratchDelay);
+        Debug.Log($"Coroutine; Stopped: {scratchDelay}");
+
+        scratchDelay = null;
+        isCoroutineRunning = false;
+    }
+
+    public IEnumerator Timer(float duration, Action action)
+    {
+        elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        action?.Invoke();
+        isCoroutineRunning = false;
     }
 }
